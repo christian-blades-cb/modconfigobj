09c644e1dc486a930e818ef08d3b19329ea76bb4
Christian Blades <cblades@pindrop.com>
2017-02-14 15:34:32 -0500

    print KVs

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -25,11 +26,44 @@ func main() {
 	buf := bufio.NewReader(fd)
 	lex := newLexer(buf)
 
+	// for {
+	// 	t := lex.nextItem()
+	// 	if t.tokenType == itemEOF {
+	// 		return
+	// 	}
+	// 	fmt.Println(t)
+	// }
+	printKVs(lex)
+}
+
+func printKVs(lex *lexer) {
+	sectionStack := []string{}
 	for {
 		t := lex.nextItem()
-		if t.tokenType == itemEOF {
+		switch t.tokenType {
+		case itemError:
+			fmt.Printf("bad token at %d", t.position)
+			os.Exit(2)
+		case itemSection:
+			depth := -1
+			for i := 0; i < len(t.value); i++ {
+				if t.value[i] == '[' {
+					depth++
+				} else {
+					break
+				}
+			}
+			cleanSectionName := strings.TrimSpace(strings.TrimLeft(strings.TrimRight(t.value, "]"), "["))
+			sectionStack = append(sectionStack[:depth], cleanSectionName)
+		case itemKeyword:
+			valueToken := lex.nextItem()
+			if valueToken.tokenType != itemValue {
+				fmt.Printf("unexpected token at %d: %v", valueToken.position, valueToken)
+				os.Exit(2)
+			}
+			fmt.Printf("%s.%s=%s\n", strings.Join(sectionStack, "."), strings.TrimSpace(t.value), strings.TrimSpace(valueToken.value))
+		case itemEOF:
 			return
 		}
-		fmt.Println(t)
 	}
 }
